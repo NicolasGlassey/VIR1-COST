9ac9636b2182c8f71f26215d06492b9f00e13e4d
Mauro Santos <maurx18@gmail.com>
2022-06-09 11:10:32 +0200

    Failed to verify customer bucket permission.

--- src/Report/Report.js
+++ src/Report/Report.js
@@ -1,6 +1,6 @@
 const {
   CostAndUsageReportServiceClient,
-   
+  PutReportDefinitionCommand
 } = require("@aws-sdk/client-cost-and-usage-report-service");
 
 module.exports = class CostReport {
@@ -9,17 +9,19 @@ module.exports = class CostReport {
     const client = new CostAndUsageReportServiceClient({region: "us-east-1"});
     try {
       const command = new PutReportDefinitionCommand({
-        PutReportDefinitionRequest: {
+        ReportDefinition: {
           ReportName: "AWS_Cost_Report",
           TimeUnit: "DAILY",
           Compression: "GZIP",
-          S3Bucket: "aws-cost-report",
-          S3Prefix: "AWS-Cost-Report",
+          S3Bucket: "vir1.budget.diduno.education",
+          S3Prefix: "",
           S3Region: "eu-west-3",
           AdditionalSchemaElements: [
             "RESOURCES",
           ],
-          ReportVersioning: "CREATE_NEW_REPORT"          
+          ReportVersioning: "CREATE_NEW_REPORT",
+          Format: "textORcsv",
+          ReportDefinition:"reptDefinition"
         },
       });
       const data = await client.send(command).then(
